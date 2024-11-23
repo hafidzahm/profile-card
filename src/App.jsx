71aace619c9e857cd0a51801870c9b0dbe0ce311
Abdul <hafizhahm123@gmail.com>
2024-11-23 12:43:04 +0700

    refactoring social media

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,26 @@ const SocialMediaButton = ({icon}) => {
     </button>
   )
 }
+
+const SocialMediaContainer = ({icon}) => {
+  return (
+    <>
+        <div className="social-buttons right">
+        <SocialMediaButton icon='facebook' />
+        <SocialMediaButton icon='twitter' />
+        <SocialMediaButton icon='instagram'/>  
+    </div>
+    <div className="social-buttons">
+              <SocialMediaButton icon='github'/>
+        <SocialMediaButton icon='linkedin'/>
+    </div>
+    </>
+  )
+}
 const CardTop = ({icon}) => {
   return (
     <div className="top" >
-      <div className="social-buttons right">
-        <SocialMediaButton icon='facebook'/>
-        <SocialMediaButton icon='twitter'/>
-        <SocialMediaButton icon='instagram'/>
-      </div>
-
-      <div className="social-buttons">
-      <SocialMediaButton icon='github'/>
-      <SocialMediaButton icon='linkedin'/>
-      </div>
+      <SocialMediaContainer icon={icon}/>
 
       <div className="text">
         <div className="name-wrapper">
@@ -28,25 +35,48 @@ const CardTop = ({icon}) => {
   );
 };
 
-const CardBottom = () => {
+const CardBottom = (nameBadge) => {
   return (
     <div className="bottom">
       <div className="desc">
+        <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea non earum
         nisi perspiciatis hic temporibus nemo ratione? Possimus, modi nostrum
         culpa recusandae, atque ut molestias ducimus architecto repellendus rem
         facere.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea non earum
+        nisi perspiciatis hic temporibus nemo ratione? Possimus, modi nostrum
+        culpa recusandae, atque ut molestias ducimus architecto repellendus rem
+        facere.
+        </p>
       </div>
-      <div className="buttons">
-        <button>phd</button>
-        <button>meowngineer</button>
-        <button>3dgeek</button>
-      </div>
+
+      <BadgeContainer nameBadge={nameBadge} />
     </div>
   );
 };
 
-const Card = ({}) => {
+const BadgeContainer = ({nameBadge}) => {
+  return (
+    <div className="buttons">
+      <BadgeItem nameBadge={'PhD'} />
+      <BadgeItem nameBadge={'Meowengineer'} />
+      <BadgeItem nameBadge={'3DGeeek'} />
+      </div>
+  )
+}
+
+const BadgeItem = ({nameBadge}) => {
+  return (
+        <button>
+          <div className="height">{nameBadge}</div>
+        </button>
+  )
+}
+
+const Card = () => {
   return (
     <div className="card">
       <CardTop  />
