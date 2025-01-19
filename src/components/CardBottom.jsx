98f4d5731b2dc70ebb1d3dc866cd054c68f07bc9
hafidzahm <hafizhahm123@gmail.com>
2025-01-19 17:30:10 +0700

    update bio

--- src/components/CardBottom.jsx
+++ src/components/CardBottom.jsx
@@ -7,10 +7,10 @@ const CardBottom = (nameBadge) => {
           Hello, I'm Hafizh. I am interested for learning FE/BE. I'm finished
           learning FE Expert Developer from DBS Foundation Coding Camp, a
           collaboration bootcamp with Dicoding Indonesia. Beside that, i also
-          learn React Basic, and Basic BE with AWS X Dicoding Indonesia too.
-          Just right now, i'm part of an awardee on Fullstack Developer from
-          Zakatloop x Hacktiv8. Click the file icon on upper left-in case you're
-          curious on my CV.
+          learn Basic React from iDCamp x Dicoding and CODEPOLITAN, and Basic BE
+          with AWS X Dicoding Indonesia too. Just right now, i'm part of an
+          awardee on Fullstack Developer from Zakatloop x Hacktiv8. Click the
+          file icon on upper left-in case you're curious on my CV.
         </p>
       </div>
 
