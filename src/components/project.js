14ddf359d91192be61911d97d7c6120f8255398c
hafidzahm <hafizhahm123@gmail.com>
2025-01-19 17:19:57 +0700

    update link project

--- src/components/project.js
+++ src/components/project.js
@@ -1,12 +1,17 @@
 const project = [
     {
+        icon: 'file',
+        href: 'https://drive.google.com/file/d/1YqNquNImCdT5TR_W6DNqCrh6WC1bPF2d/view'
+    },
+    {
         icon: 'utensils',
         href: "https://deploy-preview-39--yummy-yum.netlify.app/"
     },
     {
         icon: 'list-check',
         href: "https://notes-app-hafid.vercel.app/"
-    }
+    },
+
 ]
 
 export default project
